af403b91daff73fe3a72cc09c2b1afba5f9052e7
San Kim <waterfog9580@gmail.com>
2024-06-07 08:29:14 +0900

    leetcode 9.palindrome-number

---
+++ src/leetcode/palindrome_number.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false
+        }
+
+        let target_number = x;
+        let mut x = x;
+        let mut reversed = 0;
+        while true {
+            if x == 0 {
+                break;
+            }
+
+            let remain = x % 10;
+            reversed *= 10;
+            reversed += remain;
+            x /= 10;
+        }
+
+
+        return reversed == target_number
+    }
+}
+
+impl Solution2 for Solution {
+    fn is_palindrome(x: i32) -> bool {
+        return x.to_string().chars().rev().eq(x.to_string().chars());
+    }
+}
