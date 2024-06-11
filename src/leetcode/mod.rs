0f2ecc2dc0bac68b628d87302cad92a2cf44bee4
San Kim <waterfog9580@gmail.com>
2024-06-11 23:19:05 +0900

    leetcode 26. Remove Duplicates from Sorted Array

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -2,3 +2,4 @@ pub mod two_sum;
 pub mod palindrome_number;
 pub mod longest_common_prefix;
 pub mod merge_two_sorted_lists;
+pub mod remove_duplicates;
---
+++ src/leetcode/remove_duplicates.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+use std::i32;
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut set = HashSet::new();
+        let mut result = Vec::new();
+        nums.sort();
+
+        for i in 0..nums.len() {
+            if !set.contains(&nums[i]) {
+                set.insert(nums[i]);
+                result.push(nums[i]);
+            }
+        }
+
+        for i in 0..result.len() {
+            nums[i] = result[i];
+        }
+
+        result.len() as i32
+    }
+}
+
+/**
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        nums.dedup();
+        nums.len() as i32
+    }
+}
+*/
